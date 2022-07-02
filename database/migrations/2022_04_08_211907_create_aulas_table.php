985b4bb0ab7019f1e7f481a46376befd5abefd6f
gusgm33 <gustavogonzalesmosquera@gmail.com>
2022-07-02 14:21:53 -0400

    Deploy en heroku

--- database/migrations/2022_04_08_211907_create_aulas_table.php
+++ database/migrations/2022_04_08_211907_create_aulas_table.php
@@ -26,7 +26,31 @@ class CreateAulasTable extends Migration
         });
 
         DB::table('aulas')->insert(array(
+            'nombreAula'        =>'Auditorio',
+            'capacidadAula'     =>'300',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
             'nombreAula'        =>'690A',
+            'capacidadAula'     =>'50',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'690B',
+            'capacidadAula'     =>'40',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'690C',
             'capacidadAula'     =>'50',
             'estadoAula'        =>'Libre',
             'habilitacionAula'  =>'Habilitado',
@@ -42,8 +66,88 @@ class CreateAulasTable extends Migration
         ));
 
         DB::table('aulas')->insert(array(
+            'nombreAula'        =>'691B',
+            'capacidadAula'     =>'90',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'691C',
+            'capacidadAula'     =>'80',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'691D',
+            'capacidadAula'     =>'80',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'691E',
+            'capacidadAula'     =>'100',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'691F',
+            'capacidadAula'     =>'95',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
             'nombreAula'        =>'692A',
-            'capacidadAula'     =>'85',
+            'capacidadAula'     =>'95',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'692B',
+            'capacidadAula'     =>'80',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'692C',
+            'capacidadAula'     =>'90',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'692D',
+            'capacidadAula'     =>'90',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'692E',
+            'capacidadAula'     =>'100',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'692F',
+            'capacidadAula'     =>'95',
             'estadoAula'        =>'Libre',
             'habilitacionAula'  =>'Habilitado',
             'solicitud_id'      =>1
@@ -58,8 +162,96 @@ class CreateAulasTable extends Migration
         ));
 
         DB::table('aulas')->insert(array(
-            'nombreAula'        =>'Auditorio',
-            'capacidadAula'     =>'300',
+            'nombreAula'        =>'693B',
+            'capacidadAula'     =>'80',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'693C',
+            'capacidadAula'     =>'80',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'693D',
+            'capacidadAula'     =>'80',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'655',
+            'capacidadAula'     =>'30',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'612',
+            'capacidadAula'     =>'100',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'622',
+            'capacidadAula'     =>'110',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'623',
+            'capacidadAula'     =>'120',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'624',
+            'capacidadAula'     =>'110',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'617',
+            'capacidadAula'     =>'60',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'617B',
+            'capacidadAula'     =>'65',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'617C',
+            'capacidadAula'     =>'60',
+            'estadoAula'        =>'Libre',
+            'habilitacionAula'  =>'Habilitado',
+            'solicitud_id'      =>1
+        ));
+
+        DB::table('aulas')->insert(array(
+            'nombreAula'        =>'642',
+            'capacidadAula'     =>'70',
             'estadoAula'        =>'Libre',
             'habilitacionAula'  =>'Habilitado',
             'solicitud_id'      =>1
